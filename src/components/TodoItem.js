211be3421b6a0cc45c175a6042b81f98cfd81b46
andgarzonmal <andgarzonmal@unal.edu.co>
2022-07-02 17:04:34 -0500

    add inputs and checkboxes

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -3,7 +3,17 @@ import React, { Component } from 'react'
 export default class TodoItem extends Component {
   render() {
     return (  
-      <li>{this.props.todo.title}</li>
+      <li>
+        <input 
+          type="checkbox" 
+          checked={this.props.todo.completed} 
+          onChange={() => this.props.handleChangeProps(this.props.todo.id)} 
+        /> 
+        <button onClick={()=> this.props.deleteTodoProps(this.props.todo.id)}>
+          Delete
+        </button>
+        {this.props.todo.title}
+      </li>
     )
   }
 }
