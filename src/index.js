b4c205362a61547e98f2c56ce1392a6f74049b01
andgarzonmal <andgarzonmal@unal.edu.co>
2022-07-03 12:22:45 -0500

    import function based Components

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom"
 //component file
-import TodoContainer from "./components/TodoContainer"
+import TodoContainer from "../src/components/FunctionBased/TodoContainer"
 //stylesheet
 import "./App.css"
 
